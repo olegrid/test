b28840b4a6cb44cc2d99813715b9bcb7183e26a5
Aleh Rydzeuski <Aleh_Rydzeuski@epbygomw0194.gomel.epam.com>
2016-02-03 13:17:52 +0300

    first

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Aleh Rydzeuski on 2/3/16.
+//  Copyright © 2016 Aleh Rydzeuski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Aleh Rydzeuski on 2/3/16.
+//  Copyright © 2016 Aleh Rydzeuski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
