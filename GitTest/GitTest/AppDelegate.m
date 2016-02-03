0b0d81a6a4e43b6ffd699037665b22391d663b14
Aleh Rydzeuski <Aleh_Rydzeuski@epbygomw0194.gomel.epam.com>
2016-02-03 13:24:36 +0300

    comit 2

--- GitTest/GitTest/AppDelegate.m
+++ GitTest/GitTest/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "FirstViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FirstViewController * controller = [[FirstViewController alloc] initWithNibName:@"FirstViewController" bundle:nil];
+    UINavigationController * nController = [[UINavigationController alloc] initWithRootViewController:controller];
+    [self.window setRootViewController:nController];
+    
     return YES;
 }
 
---
+++ GitTest/GitTest/ViewControllers/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  GitTest
+//
+//  Created by Aleh Rydzeuski on 2/3/16.
+//  Copyright © 2016 Aleh Rydzeuski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
